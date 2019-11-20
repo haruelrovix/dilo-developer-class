eb081719620003a82f3e01c4ac97e331ea66c40d
haruelrovix <havit@adakerja.com>
2019-11-20 14:17:20 +0700

    Add Input component

---
+++ components/Input/index.js
@@ -0,0 +1,35 @@
+
+/**
+ * Input component
+ *
+ * For inputting text into the app via a keyboard
+ */
+
+import React from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+
+const InputComponent = (props) => {
+  const onChangeText = newValue => {
+    props.handleChange(newValue, props.name)
+  }
+
+  return (
+    <TextInput
+      style={styles.textInput}
+      onChangeText={onChangeText}
+      value={props.value}
+      placeholder={props.placeholder}
+      name={props.name}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
+})
+
+export default InputComponent
