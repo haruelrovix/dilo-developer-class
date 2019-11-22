12417b7d9ae7acc8e01a3e4c4d2f3de3f6bdc69b
haruelrovix <havit@adakerja.com>
2019-11-22 15:06:49 +0700

    Add Commits component

---
+++ components/Commits/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Commits extends Component {
+  render() {
+    return (
+      <View>
+        <Text>I'm Commits component</Text>
+      </View>
+    )
+  }
+}
+
+export default Commits;
